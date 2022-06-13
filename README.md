f260e7d09095a92ba7d392b15857469c2713f0f7
Evgeniy Popov <evgeniy.popov@amatechs.com>
2022-06-13 09:34:50 +0600

    add files

---
+++ README.md
@@ -0,0 +1,20 @@
+# ESLint Configuration
+Eslint configuration for TypeScript projects.
+
+## Installation
+
+```sh
+npm install eslint @amatechs/eslint-config-ts --save-dev
+```
+
+## Usage
+
+In `package.json` add an `eslintConfig` section:
+
+```json
+"eslintConfig": {
+	"extends": [
+		"@amatechs/eslint-config-ts"
+	]
+},
+```
---
+++ src/index.js
@@ -0,0 +1,103 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    project: '../tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:security/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        bracketSpacing: false,
+        jsxSingleQuote: false,
+        useTabs: false,
+        printWidth: 120,
+        semi: true,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        bracketSameLine: false,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'no-restricted-globals': 'off',
+    'no-nested-ternary': 'off',
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 0,
+    'react/require-default-props': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'function-declaration',
+      },
+    ],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.?(scss)$'],
+        ],
+      },
+    ],
+  },
+};
